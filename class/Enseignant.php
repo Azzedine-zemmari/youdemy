f2637e23f8212ea609de84b28182923ab3970b1f
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-01-13 22:49:38 +0100

    add register for etudiant and enseignement

--- class/Enseignant.php
+++ class/Enseignant.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require "./User.php";
+require_once __DIR__. "/./User.php";
 
 class Enseignant extends User{
     public function __construct($nom,$email,$password)
--- class/etudiant.php
+++ class/etudiant.php
@@ -1,6 +1,6 @@
 <?php 
-session_start();
-require __DIR__. "/./User.php";
+// session_start();
+require_once __DIR__. "/./User.php";
 class Etudiant extends User{
     public function __construct($nom,$email,$password)
     {
